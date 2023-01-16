090f9edbdfe3f26df74319de0f8adcf94ae56383
sflax <shachar.flax@gmail.com>
2023-01-16 20:09:25 +0200

    fixed help modal

--- src/componenets/User/UserForm.tsx
+++ src/componenets/User/UserForm.tsx
@@ -1,16 +1,104 @@
-import React, { ChangeEvent } from "react";
+import React, {
+  useReducer,
+  useState,
+  useContext,
+  useEffect,
+  useRef,
+  FormEvent,
+  ChangeEventHandler,
+} from "react";
 import Input from "./Input";
 import { User } from "../../types/User";
 import "./UserForm.scss";
+import { UserContext } from "../../context/User-context";
+
+// const userNameReducer = (state, action) => {
+//   if (action.type === "USER_INPUT") {
+//     return { value: action.val, isValid: action.val.trim().length > 0 };
+//   }
+//   if (action.type === "INPUT_BLUR") {
+//     return { value: state.value, isValid: state.value.trim().length > 0 };
+//   }
+//   return { state: "", isValid: false };
+// };
+
+// const passwordReducer = (state, action) => {
+//   if (action.type === "USER_INPUT") {
+//     return { value: action.val, isValid: action.val.trim().length > 6 };
+//   }
+//   if (action.type === "INPUT_BLUR") {
+//     return { value: state.value, isValid: state.value.trim().length > 6 };
+//   }
+//   return { state: "", isValid: false };
+// };
 
 const UserForm = () => {
+  // const [formIsValid, setFormIsValid] = useState(false);
+
+  // const [userNameState, dispatchUserName] = useReducer(userNameReducer, {
+  //   value: "",
+  //   isValid: null,
+  // });
+  // const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+  //   value: "",
+  //   isValid: null,
+  // });
+
+  // const userCtx = useContext(UserContext);
+
+  // const userNameInputRef = useRef();
+  // const passwordInputRef = useRef();
+
+  // const { isValid: userNameIsValid } = userNameState;
+  // const { isValid: passwordIsValid } = passwordState;
+
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log("Checking form validity!");
+  //     setFormIsValid(userNameIsValid && passwordIsValid);
+  //   }, 500);
+
+  //   return () => {
+  //     console.log("CLEANUP");
+  //     clearTimeout(identifier);
+  //   };
+  // }, [userNameIsValid, passwordIsValid]);
+
+  // const userNameChangeHandler = (event:key) => {
+  //   dispatchUserName({ type: "USER_INPUT", val: event.target.value });
+  // };
+
+  // const passwordChangeHandler = (event) => {
+  //   dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+  // };
+
+  // const validateUserNameHandler = () => {
+  //   dispatchUserName({ type: "INPUT_BLUR" });
+  // };
+
+  // const validatePasswordHandler = () => {
+  //   dispatchPassword({ type: "INPUT_BLUR" });
+  // };
+
+  // const submitHandler = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   if (formIsValid) {
+  //     userCtx.onLogin(userNameState.value, passwordState.value);
+  //   } else if (!userNameIsValid) {
+  //     //it's not the standard focus - see Input comp.
+  //     userNameInputRef.current.focus();
+  //   } else {
+  //     passwordInputRef.current.focus();
+  //   }
+  // };
+
   const [userName, setUsername] = React.useState("");
   const [userPassword, setUserPassword] = React.useState("");
 
-  const onChangeUserName = (event: ChangeEvent<HTMLInputElement>) => {
+  const userNameChangeHandler = (event: any) => {
     setUsername(event.target.value);
   };
-  const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+  const passwordChangeHandler = (event: any) => {
     setUserPassword(event.target.value);
   };
 
@@ -22,17 +110,25 @@ const UserForm = () => {
       <form onSubmit={onSubmit}>
         <label htmlFor="username">User name</label>
         <input
-          type="text"
-          id="username"
-          name="username"
-          onChange={onChangeUserName}
+          // ref={userNameInputRef}
+          id="userName"
+          // label="userName"
+          type="userName"
+          // isValid={userNameIsValid}
+          // value={userNameState.value}
+          onChange={userNameChangeHandler}
+          // onBlur={validateUserNameHandler}
         />
         <label htmlFor="password">Password</label>
         <input
-          type="text"
+          // ref={passwordInputRef}
           id="password"
-          name="password"
-          onChange={onChangePassword}
+          // label="Password"
+          type="password"
+          // isValid={passwordIsValid}
+          // value={passwordState.value}
+          onChange={passwordChangeHandler}
+          // onBlur={validatePasswordHandler}
         />
         <button className="login-button" type="submit" value="submit">
           Submit
--- src/context/User-context.tsx
+++ src/context/User-context.tsx
@@ -1,3 +1,44 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
-export const UserContext = createContext([] as any);
+export const UserContext = createContext({
+  isLoggedIn: false,
+  //these two are dummy functions
+  onLogout: () => {},
+  // onLogin: (userName, password) => {},
+});
+
+export const AuthContextProvider = (props: any) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        // onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
