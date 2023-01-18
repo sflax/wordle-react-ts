0792fa2a741c7158872eaa139caa3f9254e02073
sflax <shachar.flax@gmail.com>
2023-01-18 19:15:05 +0200

    form form form

--- src/componenets/User/UserForm.tsx
+++ src/componenets/User/UserForm.tsx
@@ -1,24 +1,21 @@
-import React, { FormEvent, useContext } from "react";
+import React, { useContext } from "react";
 import Input from "../UI/Input";
 import Button from "../UI/Button";
 import "./UserForm.scss";
 import { UserContext } from "../../context/User-context";
 import { Navigate, useNavigate } from "react-router-dom";
-import { User } from "../../types/User";
 
 const UserForm = () => {
   const userContext = useContext(UserContext);
 
-  // const navigate = useNavigate;
   const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const userTarget = event.currentTarget;
-    // React.FormEvent<HTMLFormElement
     const userData = {
       userName: userTarget.userName.value,
       password: userTarget.password.value,
     };
-    console.log(userData);
+    userContext.setUser({ ...userData });
   };
 
   return (
